5174296a57fd92473d528702a09d30ddde2d9ce9
Chris <157814013+cbonnin88@users.noreply.github.com>
2026-03-19 15:36:10 +0100

    Refactor project_dir path resolution
    Updated project_dir to use Path(__file__).resolve().parents[2] for better path resolution.

--- lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
+++ lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
@@ -1,11 +1,13 @@
-import os
 from pathlib import Path
 from dagster_dbt import DbtProject
 
-reporoot = os.getenv("DAGSTER_CHROOT_DIR", Path(__file__).joinpath("..", "..").resolve())
-
+# Current file is: root/lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
 lumiere_capital_project = DbtProject(
-    project_dir=Path(reporoot).resolve(),
+    # parents[2] means: 
+    # [0] is lumiere_capital_pipeline/
+    # [1] is lumiere_capital_pipeline/ (the outer one)
+    # [2] is the ROOT of the repo
+    project_dir=Path(__file__).resolve().parents[2],
 )
 
 lumiere_capital_project.prepare_if_dev()
