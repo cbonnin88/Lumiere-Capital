2f3d3308042c79858aef3f957ea73c945e7d85db
Chris <157814013+cbonnin88@users.noreply.github.com>
2026-03-19 13:17:15 +0100

    Fix project_dir path resolution in project.py

--- lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
+++ lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 lumiere_capital_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..","..").resolve(),
+    project_dir=Path(__file__).joinpath("..", "..",).resolve(),
 )
 lumiere_capital_project.prepare_if_dev()
 
