e8bd5af0046b05214eb0abfedb64fb5ddb01c7c5
Chris <157814013+cbonnin88@users.noreply.github.com>
2026-03-19 15:47:16 +0100

    Clean up comments in project.py
    Removed unnecessary comments from project.py.

--- lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
+++ lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
@@ -1,12 +1,7 @@
 from pathlib import Path
 from dagster_dbt import DbtProject
 
-# Current file is: root/lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
 lumiere_capital_project = DbtProject(
-    # parents[2] means: 
-    # [0] is lumiere_capital_pipeline/
-    # [1] is lumiere_capital_pipeline/ (the outer one)
-    # [2] is the ROOT of the repo
     project_dir=Path(__file__).resolve().parents[2],
 )
 
