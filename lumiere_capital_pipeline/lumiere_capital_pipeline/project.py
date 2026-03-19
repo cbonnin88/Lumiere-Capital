2d12ff8689f24545c73a10c890538d06587dc8cc
Chris <157814013+cbonnin88@users.noreply.github.com>
2026-03-19 13:06:53 +0100

    Refactor project_dir path in DbtProject setup
    Updated project_dir path for DbtProject initialization.

--- lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
+++ lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
@@ -3,8 +3,9 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 lumiere_capital_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", "..").resolve(),
-    packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
+    # Path.cwd() is the root of your repo in Dagster Cloud
+    # If dbt_project.yml is in the root, use this:
+    project_dir=Path(__file__).joinpath("..", "..").resolve(), 
 )
 lumiere_capital_project.prepare_if_dev()
 
