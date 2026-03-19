3285072ef88785de9f87fa0cbae1f8d210e12614
Chris <157814013+cbonnin88@users.noreply.github.com>
2026-03-19 13:07:59 +0100

    Update project.py

--- lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
+++ lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
@@ -3,8 +3,6 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 lumiere_capital_project = DbtProject(
-    # Path.cwd() is the root of your repo in Dagster Cloud
-    # If dbt_project.yml is in the root, use this:
     project_dir=Path(__file__).joinpath("..", "..").resolve(), 
 )
 lumiere_capital_project.prepare_if_dev()
