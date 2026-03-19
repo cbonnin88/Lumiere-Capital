bae77e210a69e709daed3d8ab6ecaefb863d7534
Chris <157814013+cbonnin88@users.noreply.github.com>
2026-03-19 15:26:24 +0100

    Update project directory path to use environment variable

--- lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
+++ lumiere_capital_pipeline/lumiere_capital_pipeline/project.py
@@ -1,9 +1,12 @@
+import os
 from pathlib import Path
-
 from dagster_dbt import DbtProject
 
+reporoot = os.getenv("DAGSTER_CHROOT_DIR", Path(__file__).joinpath("..", "..").resolve())
+
 lumiere_capital_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..").resolve(),
+    project_dir=Path(reporoot).resolve(),
 )
+
 lumiere_capital_project.prepare_if_dev()
 
